4800413a07d209f503dddc8f159d1df9229b3c2b
Diego Urrutia Astorga <durrutia@ucn.cl>
2017-04-11 16:28:25 -0300

    Added: Documentacion de los metodos.

--- core/src/main/java/com/durrutia/ebean/BaseModelDAO.java
+++ core/src/main/java/com/durrutia/ebean/BaseModelDAO.java
@@ -1,11 +1,15 @@
 package com.durrutia.ebean;
 
+import com.google.common.base.Preconditions;
+
 import java.util.List;
 
 import io.ebean.EbeanServer;
 import lombok.extern.slf4j.Slf4j;
 
 /**
+ * Funcionalidad comun para todos los managers.
+ *
  * @author Diego Urrutia Astorga <durrutia@ucn.cl>
  * @version 20170407172600
  */
@@ -13,20 +17,23 @@ import lombok.extern.slf4j.Slf4j;
 public final class BaseModelDAO {
 
     /**
-     * EbeanServer
+     * Conexion con la libreria Ebean
      */
     private EbeanServer ebeanServer;
 
     /**
-     *
+     * Se requere de {@link EbeanServer}.
      */
     public BaseModelDAO(final EbeanServer ebeanServer) {
+
+        Preconditions.checkNotNull(ebeanServer, "EbeanServer can't be null");
         this.ebeanServer = ebeanServer;
     }
 
     /**
+     * Dado el nombre de una clase, retorna una lista de esos objectos presentes en la base de datos.
      *
-     * @param clazz
+     * @param clazz de la clase a buscar.
      * @return the {@link List}.
      */
     public <T> List<T> findList(Class<T> clazz) {
@@ -37,10 +44,11 @@ public final class BaseModelDAO {
     }
 
     /**
+     * Obtengo un T en particular dado el nombre de su propiedad y su valor.
      *
-     * @param clazz
-     * @param propertyName
-     * @param value
+     * @param clazz        de la clase a buscar.
+     * @param propertyName de la clase.
+     * @param value        de la property.
      */
     public <T> T get(Class<T> clazz, String propertyName, Object value) {
         return this.ebeanServer.find(clazz)
@@ -53,7 +61,7 @@ public final class BaseModelDAO {
 
 
     /**
-     * Shutdown
+     * Shutdown el backend.
      */
     public void shutdown() {
 
